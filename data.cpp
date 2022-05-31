3bed2288d0cb8f7342967dfc2df4198433c8f3bb
TrashBinNp2019 <49650767+TrashBinNp2019@users.noreply.github.com>
2022-05-31 13:58:34 +0300

    Data class developed

--- data.cpp
+++ data.cpp
@@ -1,6 +1,22 @@
 #include "data.h"
 
-Data::Data()
+Data::Data(QObject *)
 {
 
+}
+
+Data *Data::operator<<( const DataElement &element )
+{
+    elements.append( element );
+    return this;
+}
+
+QList< Data::DataElement >::iterator Data::begin()
+{
+    return elements.begin();
+}
+
+QList< Data::DataElement >::iterator Data::end()
+{
+    return elements.end();
 }
--- data.h
+++ data.h
@@ -3,12 +3,32 @@
 
 #include <QObject>
 
+typedef QList< QVariant > InfoSet;
+
 class Data
 {
+    Q_OBJECT
+
 public:
-    Data();
+    struct DataElement {
+        InfoSet info;
+        QString name;
+    };
+    Data( QObject* = nullptr );
+
+    Data *operator <<(const DataElement &element );
+
+    // read
+    QList< Data::DataElement >::iterator begin();
+    QList< Data::DataElement >::iterator end();
+
+// if needed
+//
+//signals:
+//    void elementsAppended();
+
 private:
-    int data;
+    QList< DataElement > elements;
 };
 
 #endif // DATA_H
